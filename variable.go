72bc7e1c8bce857f3fe48127c56960d7499fac50
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-09-13 06:23:24 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var firstName string = "Rachmat"
+
+    var lastName string
+    lastName = "Gunawan"
+
+    fmt.Printf("halo %s %s!\n", firstName, lastName)
+}
