1db3142854d0129ee1ab1c95ad23d3df1ca9e098
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-09-14 19:06:36 +0700

    Data Type

---
+++ data_type.go
@@ -0,0 +1,28 @@
+// Data Type Numeric Non Decimal
+var positiveNumber unit8 = 69
+var negativeNumber = -23442364
+
+fmt.Printf("Positive Number : %d\n", positiveNumber)
+fmt.Printf("Negative Number : %d\n", negativeNumber)
+
+//Data Type Numeric Decimal
+var decimalNumber = 3.45
+
+fmt.Printf("Number Decimal : %f\n". decimalNumber)
+fmt.Printf("Number Decimal : %.3f\n", decimalNumber)
+
+//Data Type Boolean
+var exist bool = true
+fmt.Printf("exist? %t \n", exist)
+
+//Data Type String
+var message string = "haihai"
+fmt.Printf("message: %s \n", message)
+
+var message1 = "My name is Rachmat Gunawan.
+Nice To Meet You.
+Lets Go"
+fmt.Printf(message1)
+
+
+
